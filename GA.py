5911eb3c9524bc0b078562397b7499db6e7e7094
hsuallan <33412402+hsuallan@users.noreply.github.com>
2020-05-07 22:35:25 +0800

    first commit

---
+++ GA.py
@@ -0,0 +1,83 @@
+import math 
+import random
+import sys
+class cfg():
+    def __init__(self,rangeX,rangeY,populationLen,dnaLen):
+        self.rangeX = rangeX
+        self.rangeY = rangeY
+        self.populationLen = populationLen
+        self.dnaLen = dnaLen
+    pass
+def getfitness(x,y):
+    return 21.5+(x*math.sin(4*math.pi*x))+(y*math.sin(20*math.pi*y))
+
+def randomDNA(len):
+    arr = []
+    for i in range(0,len):
+        arr.append(random.randrange(2))
+    return arr
+def geneToNumber(cfg,gene):
+    x = 0
+    y = 0
+    for index,item in enumerate(gene):
+        if index < len(gene)/2:
+            x += item*math.pow(2,index)
+        else:
+            y += item*math.pow(2,index-len(gene)/2)
+    x = cfg.rangeX[0]+((cfg.rangeX[1]-cfg.rangeX[0])/math.pow(2,len(gene)/2)*x)
+    y = cfg.rangeY[0]+((cfg.rangeY[1]-cfg.rangeY[0])/math.pow(2,len(gene)/2)*y)
+    return [x,y]
+def population(cfg):
+    pop = []
+    for item in range(0,cfg.populationLen):
+        pop.append(randomDNA(cfg.dnaLen))
+    return pop
+def crossover(f,m):
+    # f/m is gene
+    child= []
+    for index in range(0,len(f)):
+        if random.randrange(2) == 1:
+            child.append(f[index])
+        else:
+            child.append(m[index])
+    return child
+def mutate(gene):
+    rate = 25
+    for index in range(0,len(gene)):
+        if random.randrange(1,100) < rate:
+            gene[index] = 1 if  gene[index] == 0 else 1
+    return gene
+def findMax(d1):
+    d2 = d1.copy()
+    v = list(d2.values())
+    k = list(d2.keys())
+    return k[v.index(max(v))]
+def selection(cfg,pop,times,wwwww):
+    all = dict(wwwww)
+    for index,item in enumerate(pop):
+        if index in all:
+            pass
+        else:
+            gene = geneToNumber(cfg,item)
+            x = gene[0]
+            y = gene[1]
+            all[index] = getfitness(x,y)
+    maxIndex = findMax(all)
+    father = pop[maxIndex]
+    if times%1000 == 0:
+        gene = geneToNumber(cfg,father)
+        x = gene[0]
+        y = gene[1]
+        print(times,'---->',x,y,all[maxIndex])
+    return [father,all]    
+if __name__ == "__main__":
+    if sys.argv[1] == "help":
+        print('python GA.py {{ population }} {{ genelength }} {{ generation }}')
+    else:
+        cfg = cfg([-3.0,12.1],[4.1,5.8],int(sys.argv[1]),int(sys.argv[2]))
+        #selection(cfg,population(cfg))
+        pop = population(cfg)
+        best = [000,{}]
+        for times in range(int(sys.argv[3])):
+            best = selection(cfg,pop,times,best[1])
+            pop.append(mutate(crossover(best[0],pop[random.randrange(0,len(pop))])))
